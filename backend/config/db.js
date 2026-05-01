30dbe5ad9276d5bcd9fc7fa4b260c447e0e56fcb
mobasshirhussain2772 <akhtarsana0032@gmail.com>
2026-05-01 16:55:49 +0530

    Final MERN project with MySQL and Dashboard

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,17 @@
 const mysql = require("mysql2");
 
+// Railway automatically ye variables provide karta hai
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mysql123",
-  database: "task_manager"
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "mysql123",
+  database: process.env.MYSQLDATABASE || "task_manager",
+  port: process.env.MYSQLPORT || 3306
 });
 
 db.connect((err) => {
-  if (err) {
-    console.log("DB Error:", err);
-  } else {
-    console.log("MySQL Connected...");
-  }
+  if (err) console.log("DB Connection Error:", err);
+  else console.log("MySQL Connected to Railway/Local...");
 });
 
 module.exports = db;
